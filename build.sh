bc43fa241dad144e49285c1e2165b556a8477578
Silica163 <96188282+Silica163@users.noreply.github.com>
2025-07-24 11:43:51 +0700

    Static link the exec.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
-echo gcc -o 2048 2048.c
-time gcc -o 2048 2048.c
+echo gcc -static -o 2048 2048.c
+time gcc -static -o 2048 2048.c
 echo
-echo x86_64-w64-mingw32-gcc -fexec-charset=cp437 2048.c -o 2048.exe
-time x86_64-w64-mingw32-gcc -fexec-charset=cp437 2048.c -o 2048.exe
+echo x86_64-w64-mingw32-gcc -static -fexec-charset=cp437 2048.c -o 2048.exe
+time x86_64-w64-mingw32-gcc -static -fexec-charset=cp437 2048.c -o 2048.exe
